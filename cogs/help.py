d6a216747aee3663e24908e39ee2bff15374ac4f
MySekwel <mysekwel@gmail.com>
2021-01-27 00:44:04 +0800

    Help Remaster (Page 1)

--- cogs/help.py
+++ cogs/help.py
@@ -13,7 +13,7 @@ Module Dependencies:
 import asyncio
 import datetime
 
-from discord import Embed, Colour
+from discord import Embed, Colour, Color
 from discord.ext import commands
 from discord.ext.commands import BucketType, cooldown, CommandOnCooldown
 
@@ -41,20 +41,77 @@ class Help(commands.Cog):
         if category is None:
             await ctx.channel.trigger_typing()
             await asyncio.sleep(2)
-            description = ""
+            description = \
+                "Use `!help [command]` for more info on a *command*.\n\
+                Use `!help [category]` for more info on a *category*.\n\
+                For more help, join the official bot support server: https://discord.gg/EpDbtGbDv6"
             embed = Embed(
                 title="Help",
                 description=description,
                 colour=Colour.random()
             )
             embed.set_thumbnail(url="https://i.imgur.com/bObV3r5.png")
-            time = datetime.datetime.now()
-            embed.set_footer(text=time.strftime("**Page** 1 | %B %d, %Y | %I:%M %p"))
+
+            empty_field(embed)
+            empty_field(embed)
+            empty_field(embed)
+
+            embed.add_field(
+                name="Categories:",
+                value=emoji_dictionary.special['empty'],
+                inline=True
+            )
+            empty_field(embed)
+            empty_field(embed)
             embed.add_field(
                 name="Category: General",
-                value="**Usage:** Show bot commands"
+                value="**Description:** Shows all available commands that are uncategorized.",
+                inline=True
             )
-            ctx.send(embed=embed)
+            embed.add_field(
+                name="Category: Casino",
+                value="**Description:** Casino System, all gambling related commands, for people who loves risks!",
+                inline=True
+            )
+            embed.add_field(
+                name="Category: Labor",
+                value="**Description:** Labor System, money-earning commands, for industrious people.",
+                inline=True
+            )
+            embed.add_field(
+                name="Category: Economy",
+                value="**Description:** Economy System, commands related to the economy like buying & selling of "
+                      "stuffs.",
+                inline=True
+            )
+            embed.add_field(
+                name="Category: Reaction",
+                value="**Description:** Reaction System, how to get reaction stats like reputation, love and more.",
+                inline=True
+            )
+            embed.add_field(
+                name="Category: Donation",
+                value="**Description:** Want to donate? Here's how.",
+                inline=True
+            )
+            empty_field(embed)
+            empty_field(embed)
+            empty_field(embed)
+            embed.add_field(
+                name="Commands:",
+                value=emoji_dictionary.special['empty'],
+                inline=True
+            )
+            empty_field(embed)
+            empty_field(embed)
+            embed.add_field(
+                name="Command: !help",
+                value="**Description:** Show the help menu.\n**Usage:** `!help [command/category]`",
+                inline=True
+            )
+            time = datetime.datetime.now()
+            embed.set_footer(text=time.strftime("Page 1 | %B %d, %Y | %I:%M %p"))
+            await ctx.send(embed=embed)
 
             return
         if category.casefold() == "general":
@@ -65,6 +122,19 @@ class Help(commands.Cog):
             pass
         elif category.casefold() == "economy":
             pass
+        elif category.casefold() == 'reaction':
+            pass
+        elif category.casefold() == 'donate':
+            pass
+        else:
+            embed = Embed(
+                title="Error",
+                description="That category doesn't exist, type `!help` to see the available categories.",
+                colour=Color.red()
+            )
+            await ctx.channel.trigger_typing()
+            await asyncio.sleep(2)
+            await ctx.reply(embed=embed)
 
     @help.error
     async def help_error(self, ctx, exc):
